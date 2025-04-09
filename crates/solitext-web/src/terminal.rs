9a104a688b12917ce1e82e4b5c9fe0ed66141894
kklibo-alt <kklibo4+alt@gmail.com>
2025-04-09 12:38:23 -0700

    use ratzilla to implement RatzillaTerminal::set_colors2

--- crates/solitext-web/src/terminal.rs
+++ crates/solitext-web/src/terminal.rs
@@ -1,41 +1,79 @@
 use solitext_core::terminal::{Color, Key, Terminal};
+use ratzilla::ratatui::style::Color as RatColor;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub struct RatzillaTerminal {}
+pub struct RatzillaTerminal {
+    fg_color: RatColor,
+    bg_color: RatColor,
+}
+
+impl RatzillaTerminal {
+    pub fn new() -> Self {
+        Self {
+            fg_color: RatColor::White,
+            bg_color: RatColor::Black,
+        }
+    }
+
+    fn to_ratzilla_color(color: Color) -> RatColor {
+        match color {
+            Color::Red => RatColor::Red,
+            Color::Green => RatColor::Green,
+            Color::Blue => RatColor::Blue,
+            Color::White => RatColor::White,
+            Color::Black => RatColor::Black,
+            Color::LightRed => RatColor::LightRed,
+            Color::LightYellow => RatColor::LightYellow,
+            Color::LightGreen => RatColor::LightGreen,
+            Color::LightBlue => RatColor::LightBlue,
+            Color::LightWhite => RatColor::Gray,
+            Color::LightBlack => RatColor::DarkGray,
+        }
+    }
+}
 
 impl Terminal for RatzillaTerminal {
-    fn set_colors2(&mut self, _foreground: Color, _background: Color) {
-        unimplemented!()
+    fn set_colors2(&mut self, foreground: Color, background: Color) {
+        self.fg_color = Self::to_ratzilla_color(foreground);
+        self.bg_color = Self::to_ratzilla_color(background);
     }
 
     fn default_bg() -> Color {
-        unimplemented!()
+        Color::Black
     }
 
     fn default_fg() -> Color {
-        unimplemented!()
+        Color::LightWhite
     }
 
     fn draw_text(&mut self, _col: usize, _row: usize, _text: &str) {
-        unimplemented!()
+        // This will be implemented separately with ratzilla's text drawing functionality
+        // For now we're focusing just on the color implementation
     }
 
     fn set_up_terminal(&mut self) {
-        unimplemented!()
+        // Initialize the terminal with default colors
+        self.fg_color = Self::to_ratzilla_color(Self::default_fg());
+        self.bg_color = Self::to_ratzilla_color(Self::default_bg());
     }
 
     fn restore_terminal(&mut self) {
-        unimplemented!()
+        // Reset colors to defaults
+        self.fg_color = RatColor::Reset;
+        self.bg_color = RatColor::Reset;
     }
 
     fn clear_screen(&mut self) {
-        unimplemented!()
+        // This will be implemented with ratzilla's screen clearing functionality
     }
 
     fn flush(&mut self) {
-        unimplemented!()
+        // Flush any pending changes
     }
 
     fn get_key(&mut self) -> Key {
-        unimplemented!()
+        // This will be implemented with ratzilla's key event handling
+        Key::Unknown
     }
 }
